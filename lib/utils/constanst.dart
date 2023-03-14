f7087513bf6c1dfa1bbd7f5c264780764d8ba3d8
Gian Felipe da Silva <gian.silva@cialne.com.br>
2023-03-14 15:05:14 -0300

    first commit

---
+++ lib/utils/constanst.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:ftoast/ftoast.dart';
+import 'package:panara_dialogs/panara_dialogs.dart';
+
+///
+import '../utils/strings.dart';
+import '../../main.dart';
+
+/// Empty Title & Subtite TextFields Warning
+emptyFieldsWarning(context) {
+  return FToast.toast(
+    context,
+    msg: MyString.oopsMsg,
+    subMsg: "Você deve preencher todos os campos!",
+    corner: 20.0,
+    duration: 2000,
+    padding: const EdgeInsets.all(20),
+  );
+}
+
+/// Nothing Enter When user try to edit the current tesk
+nothingEnterOnUpdateTaskMode(context) {
+  return FToast.toast(
+    context,
+    msg: MyString.oopsMsg,
+    subMsg: "Você deve editar as tarefas e depois tentar atualizá-las!",
+    corner: 20.0,
+    duration: 3000,
+    padding: const EdgeInsets.all(20),
+  );
+}
+
+/// No task Warning Dialog
+dynamic warningNoTask(BuildContext context) {
+  return PanaraInfoDialog.showAnimatedGrow(
+    context,
+    title: MyString.oopsMsg,
+    message:
+        "Não há tarefa para excluir! \n Tente adicionar algumas e depois tente excluí-las!",
+    buttonText: "Okay",
+    onTapDismiss: () {
+      Navigator.pop(context);
+    },
+    panaraDialogType: PanaraDialogType.warning,
+  );
+}
+
+/// Delete All Task Dialog
+dynamic deleteAllTask(BuildContext context) {
+  return PanaraConfirmDialog.show(
+    context,
+    title: MyString.areYouSure,
+    message:
+        "Você realmente quer excluir todas as tarefas? Você não será capaz de desfazer essa ação!",
+    confirmButtonText: "Sim",
+    cancelButtonText: "Não",
+    onTapCancel: () {
+      Navigator.pop(context);
+    },
+    onTapConfirm: () {
+      BaseWidget.of(context).dataStore.box.clear();
+      Navigator.pop(context);
+    },
+    panaraDialogType: PanaraDialogType.error,
+    barrierDismissible: false,
+  );
+}
+
+/// lottie asset address
+String lottieURL = 'assets/lottie/1.json';
---
+++ lib/utils/strings.dart
@@ -0,0 +1,17 @@
+class MyString {
+  static const String mainTitle = "Tarefas";
+  static const String deletedTask = "Esta tarefa foi deletada";
+  static const String doneAllTask = "Voce concluiu todas as tarefas👌";
+  static const String addNewTask = "Cadastro ";
+  static const String updateCurrentTask = "Atualizar ";
+  static const String taskStrnig = "Tarefa";
+  static const String titleOfTitleTextField = "O que você está planejando😇?";
+  static const String addNote = 'Adicione uma label';
+  static const String timeString = "Horas";
+  static const String dateString = "Data";
+  static const String deleteTask = "Delete a tarefa";
+  static const String addTaskString = "Adicionar Tarefa";
+  static const String updateTaskString = "Atualizar Tarefa";
+  static const String oopsMsg = "Oops!";
+  static const String areYouSure = "Você tem certeza?";
+}
